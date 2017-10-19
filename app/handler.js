e192eb561c3d96ebc192fbf93aad9843b4f666a5
pj <starsinmypockets@gmail.com>
2017-10-19 18:36:05 -0400

    Adds fetchToS3 function

--- app/handler.js
+++ app/handler.js
@@ -1,32 +1,59 @@
 'use strict'
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-  callback(null, response)
+////////////////////
+// LAMBDA EXPORTS /
+//////////////////
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-}
-
+/**
+ * Validate a schema using nodejs JSONSchema implementation
+ **/
 module.exports.validateSchema = (event, context, callback) => {
   const res = _validateSchema(event.payload, event.validator)
 
   callback(null, res)
 }
 
+/**
+ * Fetch a resource using aws sdk and 
+ * upload to specified s3 bucket
+ **/
+module.exports.fetchToS3 = (event, context, callback) => {
+  fetch(event.url)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(new Error(
+        `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+    })
+    .then(response => response.buffer())
+    .then(buffer => (
+      s3.putObject({
+        Bucket: event.bucket,
+        Key: event.key,
+        Body: buffer,
+      }).promise()
+    ))
+    .then(v => callback(null, v), callback);
+};
 
-const _validateSchema = (payload, validator) => {
+/**
+ * Given a valid tag get s3 metadata for a dataResource
+ *
+ */
+module.exports.getS3ResourceInfo = (event, context, callback) => {
+  // grab it
+}
+
+///////////////////////
+// Units for testing /
+/////////////////////
+const _validateSchema = module.exports._validateSchema = (payload, validator) => {
   const Validator = require('jsonschema')
   const v = new Validator.Validator()
   return v.validate(payload, validator)
 }
-
-// Exports for testing
-module.exports._validateSchema = _validateSchema
